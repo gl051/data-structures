813d34cc3970d730e13da4d87f82bf422c629b6e
Gianluca <gl051@biccari.com>
2016-04-08 17:37:44 -0700

    Add initialization and generic configuration

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+    Make the directory loadable as a module
+"""
+
+# 1 - Put your generic initialization, for example:
+# import logging.config
+# logging.config.dictConfig(Your_logging_config)
+
+
+# 2 - For convenience add the import of everything from the local files,
+# so that the user will not have to know the file name but only the class
+from linkedlist import *
+from hashtable import *
+from array import *
+from heap import *
+from queue import *
+from stack import *
