1980de8d7c7213ade7d810f869b9b6e0ad06c676
Gianluca <gl051@biccari.com>
2018-03-30 08:54:06 -0700

    Fix header

--- circular_buffer.py
+++ circular_buffer.py
@@ -1,7 +1,9 @@
-# Implement a circular buffer of size N. 
-# Allow to append, remove and list the content of the buffer
-# New items are appended to the end, when the number of elements in the buffer exceeds the defined size, 
-# the older elements are overwritten
+"""
+    Implement a circular buffer of size N. 
+    Allow to append, remove and list the content of the buffer
+    New items are appended to the end, when the number of elements in the buffer exceeds the defined size, 
+    the older elements are overwritten
+"""
 
 class CircularBuffer():
     def __init__(self, size):
